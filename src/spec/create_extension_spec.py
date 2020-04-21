3c3158e1196cab6fb243ddc72ee66e56d289d7b1
!git for-each-ref --format='%(refname:short)' `git symbolic-ref HEAD` <ben.dichter@gmail.com>
2020-04-21 18:01:29 -0400

    generate schema

--- src/spec/create_extension_spec.py
+++ src/spec/create_extension_spec.py
@@ -2,14 +2,13 @@
 
 import os.path
 
-from pynwb.spec import NWBNamespaceBuilder, export_spec, NWBGroupSpec, NWBAttributeSpec
-# TODO: import the following spec classes as needed
-# from pynwb.spec import NWBDatasetSpec, NWBLinkSpec, NWBDtypeSpec, NWBRefSpec
+from pynwb.spec import NWBNamespaceBuilder, export_spec, NWBGroupSpec
+from hdmf.spec import GroupSpec, NamespaceBuilder
 
 
 def main():
     # these arguments were auto-generated from your cookiecutter inputs
-    ns_builder = NWBNamespaceBuilder(
+    ns_builder = NamespaceBuilder(
         doc="""Provides a table for linking free-form terms in an NWB file to an ontological terms""",
         name="""ndx-ontology-table""",
         version="""0.1.0""",
@@ -17,31 +16,70 @@ def main():
         contact=list(map(str.strip, """ben.dichter@catalystneuro.com""".split(',')))
     )
 
-    # TODO: specify the neurodata_types that are used by the extension as well
-    # as in which namespace they are found
-    # this is similar to specifying the Python modules that need to be imported
-    # to use your new data types
-    ns_builder.include_type('ElectricalSeries', namespace='core')
+    ns_builder.include_type('DynamicTable', namespace='hdmf-common')
+    #ns_builder.include_type('LabMetaData', namespace='core')
 
-    # TODO: define your new data types
-    # see https://pynwb.readthedocs.io/en/latest/extensions.html#extending-nwb
-    # for more information
-    tetrode_series = NWBGroupSpec(
-        neurodata_type_def='TetrodeSeries',
-        neurodata_type_inc='ElectricalSeries',
-        doc=('An extension of ElectricalSeries to include the tetrode ID for '
-             'each time series.'),
-        attributes=[
-            NWBAttributeSpec(
-                name='trode_id',
-                doc='The tetrode ID.',
-                dtype='int32'
-            )
-        ],
+
+    """
+    OntologicalTerms = NWBGroupSpec(
+        name='OntologicalTerms',
+        doc='Holds ontology metadata',
+        neurodata_type_def='OntologicalTerms',
+        neurodata_type_inc='LabMetaData',
+    )
+    """
+
+    OntologyTable = GroupSpec(
+        name='OntologyTable',
+        data_type_def='OntologyTable',
+        data_type_inc='DynamicTable',
+        doc='Maps terms in an NWB file to ontological terms'
     )
 
-    # TODO: add all of your new data types to this list
-    new_data_types = [tetrode_series]
+    OntologyTable.add_dataset(
+        name='object_id',
+        data_type_inc='VectorData',
+        doc='uuid of neurodata_type',
+        shape=(None,),
+        dtype='text',
+        dims=('nterms',)
+    )
+
+    OntologyTable.add_dataset(
+        name='field_name',
+        data_type_inc='VectorData',
+        doc='neurodata_type field',
+        shape=(None,),
+        dtype='text',
+        dims=('nterms',)
+    )
+
+    OntologyTable.add_dataset(
+        name='field_value',
+        data_type_inc='VectorData',
+        doc='value of field',
+        shape=(None,),
+        dims=('nterms',)
+    )
+
+    OntologyTable.add_dataset(
+        name='uri',
+        data_type_inc='VectorData',
+        doc='uri to match with',
+        shape=(None,),
+        dtype='text',
+        dims=('nterms',)
+    )
+
+    """
+    OntologyTable.add_group(
+        neurodata_type_inc='OntologyLookup',
+        quantity=1,
+        doc='Maps terms in an NWB file to ontological terms'
+    )
+    """
+
+    new_data_types = [OntologyTable]
 
     # export the spec to yaml files in the spec folder
     output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'spec'))
